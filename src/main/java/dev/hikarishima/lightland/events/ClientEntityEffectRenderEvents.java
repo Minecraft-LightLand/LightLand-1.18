8cd64e72ba328f102ee15c8592d8341c3833dad0
lcy0x1 <arthurlcy0x1@gmail.com>
2022-02-05 19:18:19 -0800

    fix arcane icon render

--- src/main/java/dev/hikarishima/lightland/events/ClientEntityEffectRenderEvents.java
+++ src/main/java/dev/hikarishima/lightland/events/ClientEntityEffectRenderEvents.java
@@ -1,17 +1,41 @@
 package dev.hikarishima.lightland.events;
 
+import com.mojang.blaze3d.systems.RenderSystem;
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.blaze3d.vertex.VertexConsumer;
 import dev.hikarishima.lightland.content.archery.feature.bow.GlowTargetAimFeature;
 import dev.hikarishima.lightland.content.archery.item.GenericBowItem;
+import dev.hikarishima.lightland.content.common.effect.EmeraldPopeEffect;
+import dev.hikarishima.lightland.content.common.render.LLRenderState;
+import dev.hikarishima.lightland.init.LightLand;
+import dev.hikarishima.lightland.init.registrate.ParticleRegistrate;
 import dev.hikarishima.lightland.init.registrate.VanillaMagicRegistrate;
 import dev.lcy0x1.base.Proxy;
+import net.minecraft.client.Camera;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.LevelRenderer;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.entity.EntityRenderDispatcher;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.Mth;
+import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.Vec3;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.client.event.FOVModifierEvent;
+import net.minecraftforge.client.event.RenderLevelLastEvent;
+import net.minecraftforge.client.event.RenderLivingEvent;
 import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+
+import java.util.ArrayList;
+import java.util.Map;
 
 public class ClientEntityEffectRenderEvents {
 
@@ -98,6 +122,106 @@ public class ClientEntityEffectRenderEvents {
             float p = bow.config.fov_time();
             event.setNewfov(f * (1 - Math.min(1, i / p) * bow.config.fov()));
         }
+    }
+
+    private record DelayedEntityRender(LivingEntity entity, ResourceLocation rl) {
+
+    }
+
+    public static final ResourceLocation RL_ENTITY_BODY_ICON = new ResourceLocation(LightLand.MODID, "textures/arcane_icon.png");
+    public static final ResourceLocation WATER_TRAP_ICON = new ResourceLocation(LightLand.MODID, "textures/water_trap_icon.png");
+
+    private static final ArrayList<DelayedEntityRender> ICONS = new ArrayList<>();
+
+    @OnlyIn(Dist.CLIENT)
+    @SubscribeEvent
+    public static void levelRenderLast(RenderLevelLastEvent event) {
+        LevelRenderer renderer = event.getLevelRenderer();
+        MultiBufferSource.BufferSource buffers = Minecraft.getInstance().renderBuffers().bufferSource();
+        Camera camera = Minecraft.getInstance().gameRenderer.getMainCamera();
+        PoseStack stack = event.getPoseStack();
+        RenderSystem.disableDepthTest();
+        for (DelayedEntityRender icon : ICONS) {
+            renderIcon(icon.entity(), stack, buffers, icon.rl(), event.getPartialTick(), camera, renderer.entityRenderDispatcher);
+        }
+        buffers.endBatch();
+        ICONS.clear();
+    }
+
+    @OnlyIn(Dist.CLIENT)
+    @SubscribeEvent
+    public static void onLivingEntityRender(RenderLivingEvent.Post<?, ?> event) {
+        LivingEntity entity = event.getEntity();
+        if (EffectSyncEvents.EFFECT_MAP.containsKey(entity.getUUID())) {
+            Map<MobEffect, Integer> map = EffectSyncEvents.EFFECT_MAP.get(entity.getUUID());
+            if (map.containsKey(VanillaMagicRegistrate.ARCANE.get())) {
+                ICONS.add(new DelayedEntityRender(entity, RL_ENTITY_BODY_ICON));
+            }
+            if (map.containsKey(VanillaMagicRegistrate.WATER_TRAP.get())) {
+                ICONS.add(new DelayedEntityRender(entity, WATER_TRAP_ICON));
+            }
+            if (map.containsKey(VanillaMagicRegistrate.EMERALD.get())) {
+                if (!Minecraft.getInstance().isPaused() && entity != Proxy.getClientPlayer()) {
+                    int lv = map.get(VanillaMagicRegistrate.EMERALD.get());
+                    int r = EmeraldPopeEffect.RADIUS * (1 + lv);
+                    int count = (1 + lv) * (1 + lv) * 4;
+                    for (int i = 0; i < count; i++) {
+                        addParticle(entity.level, entity.position(), r);
+                    }
+                }
+            }
+        }
+    }
+
+    @OnlyIn(Dist.CLIENT)
+    private static void renderIcon(LivingEntity entity, PoseStack matrix, MultiBufferSource buffer, ResourceLocation rl,
+                                   float partial, Camera camera, EntityRenderDispatcher dispatcher) {
+        float f = entity.getBbHeight() / 2;
+
+        double x0 = Mth.lerp(partial, entity.xOld, entity.getX());
+        double y0 = Mth.lerp(partial, entity.yOld, entity.getY());
+        double z0 = Mth.lerp(partial, entity.zOld, entity.getZ());
+        Vec3 offset = dispatcher.getRenderer(entity).getRenderOffset(entity, partial);
+        Vec3 cam_pos = camera.getPosition();
+        double d2 = x0 - cam_pos.x + offset.x();
+        double d3 = y0 - cam_pos.y + offset.y();
+        double d0 = z0 - cam_pos.z + offset.z();
+
+        matrix.pushPose();
+        matrix.translate(d2, d3 + f, d0);
+        matrix.mulPose(camera.rotation());
+        PoseStack.Pose entry = matrix.last();
+        VertexConsumer ivertexbuilder = buffer.getBuffer(LLRenderState.get2DIcon(rl));
+        iconVertex(entry, ivertexbuilder, 0.5f, -0.5f, 0, 1);
+        iconVertex(entry, ivertexbuilder, -0.5f, -0.5f, 1, 1);
+        iconVertex(entry, ivertexbuilder, -0.5f, 0.5f, 1, 0);
+        iconVertex(entry, ivertexbuilder, 0.5f, 0.5f, 0, 0);
+        matrix.popPose();
+    }
+
+    @OnlyIn(Dist.CLIENT)
+    private static void iconVertex(PoseStack.Pose entry, VertexConsumer builder, float x, float y, float u, float v) {
+        builder.vertex(entry.pose(), x, y, 0)
+                .uv(u, v)
+                .normal(entry.normal(), 0.0F, 1.0F, 0.0F)
+                .endVertex();
+    }
+
+    private static void addParticle(Level w, Vec3 vec, int r) {
+        float tpi = (float) (Math.PI * 2);
+        Vec3 v0 = new Vec3(0, r, 0);
+        Vec3 v1 = v0.xRot(tpi / 3).yRot((float) (Math.random() * tpi));
+        float a0 = (float) (Math.random() * tpi);
+        float b0 = (float) Math.acos(2 * Math.random() - 1);
+        v0 = v0.xRot(a0).yRot(b0);
+        v1 = v1.xRot(a0).yRot(b0);
+        w.addAlwaysVisibleParticle(ParticleRegistrate.EMERALD.get(),
+                vec.x + v0.x,
+                vec.y + v0.y,
+                vec.z + v0.z,
+                vec.x + v1.x,
+                vec.y + v1.y,
+                vec.z + v1.z);
     }
 
 }
--- src/main/java/dev/hikarishima/lightland/events/EffectSyncEvents.java
+++ src/main/java/dev/hikarishima/lightland/events/EffectSyncEvents.java
@@ -1,28 +1,14 @@
 package dev.hikarishima.lightland.events;
 
-import com.mojang.blaze3d.vertex.PoseStack;
-import com.mojang.blaze3d.vertex.VertexConsumer;
-import dev.hikarishima.lightland.content.common.effect.EmeraldPopeEffect;
-import dev.hikarishima.lightland.content.common.render.LLRenderState;
 import dev.hikarishima.lightland.init.LightLand;
-import dev.hikarishima.lightland.init.registrate.ParticleRegistrate;
 import dev.hikarishima.lightland.init.registrate.VanillaMagicRegistrate;
 import dev.hikarishima.lightland.network.packets.EffectToClient;
-import dev.lcy0x1.base.Proxy;
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.renderer.MultiBufferSource;
-import net.minecraft.client.renderer.entity.EntityRenderDispatcher;
-import net.minecraft.client.renderer.entity.LivingEntityRenderer;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.effect.MobEffect;
-import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.level.Level;
-import net.minecraft.world.phys.Vec3;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
-import net.minecraftforge.client.event.RenderLivingEvent;
 import net.minecraftforge.event.entity.living.PotionEvent;
 import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -31,43 +17,14 @@ import java.util.*;
 
 public class EffectSyncEvents {
 
-    public static final ResourceLocation RL_ENTITY_BODY_ICON = new ResourceLocation(LightLand.MODID, "textures/arcane_icon.png");
-    public static final ResourceLocation WATER_TRAP_ICON = new ResourceLocation(LightLand.MODID, "textures/water_trap_icon.png");
-
-    private static final Map<UUID, Map<MobEffect, Integer>> EFFECT_MAP = new HashMap<>();
-    private static final Set<MobEffect> TRACKED = new HashSet<>();
+    public static final Map<UUID, Map<MobEffect, Integer>> EFFECT_MAP = new HashMap<>();
+    public static final Set<MobEffect> TRACKED = new HashSet<>();
 
     public static void init() {
         TRACKED.add(VanillaMagicRegistrate.ARCANE.get());
         TRACKED.add(VanillaMagicRegistrate.WATER_TRAP.get());
         TRACKED.add(VanillaMagicRegistrate.FLAME.get());
         TRACKED.add(VanillaMagicRegistrate.EMERALD.get());
-    }
-
-    @OnlyIn(Dist.CLIENT)
-    @SubscribeEvent
-    public static void onLivingEntityRender(RenderLivingEvent.Post<?, ?> event) {
-        LivingEntity entity = event.getEntity();
-        LivingEntityRenderer<?, ?> renderer = event.getRenderer();
-        if (EFFECT_MAP.containsKey(entity.getUUID())) {
-            Map<MobEffect, Integer> map = EFFECT_MAP.get(entity.getUUID());
-            if (map.containsKey(VanillaMagicRegistrate.ARCANE.get())) {
-                renderIcon(entity, event.getPoseStack(), event.getMultiBufferSource(), renderer.entityRenderDispatcher, RL_ENTITY_BODY_ICON);
-            }
-            if (map.containsKey(VanillaMagicRegistrate.WATER_TRAP.get())) {
-                renderIcon(entity, event.getPoseStack(), event.getMultiBufferSource(), renderer.entityRenderDispatcher, WATER_TRAP_ICON);
-            }
-            if (map.containsKey(VanillaMagicRegistrate.EMERALD.get())) {
-                if (!Minecraft.getInstance().isPaused() && entity != Proxy.getClientPlayer()) {
-                    int lv = map.get(VanillaMagicRegistrate.EMERALD.get());
-                    int r = EmeraldPopeEffect.RADIUS * (1 + lv);
-                    int count = (1 + lv) * (1 + lv) * 4;
-                    for (int i = 0; i < count; i++) {
-                        addParticle(entity.level, entity.position(), r);
-                    }
-                }
-            }
-        }
     }
 
     @OnlyIn(Dist.CLIENT)
@@ -149,46 +106,6 @@ public class EffectSyncEvents {
 
     private static void onEffectDisappear(MobEffect eff, LivingEntity e) {
         new EffectToClient(e.getUUID(), eff, false, 0).toTrackingPlayers(e);
-    }
-
-    @OnlyIn(Dist.CLIENT)
-    private static void renderIcon(Entity entity, PoseStack matrix, MultiBufferSource buffer, EntityRenderDispatcher manager, ResourceLocation rl) {
-        float f = entity.getBbHeight() / 2;
-        matrix.pushPose();
-        matrix.translate(0, f, 0);
-        matrix.mulPose(manager.cameraOrientation());
-        PoseStack.Pose entry = matrix.last();
-        VertexConsumer ivertexbuilder = buffer.getBuffer(LLRenderState.get2DIcon(rl));
-        iconVertex(entry, ivertexbuilder, 0.5f, -0.5f, 0, 1);
-        iconVertex(entry, ivertexbuilder, -0.5f, -0.5f, 1, 1);
-        iconVertex(entry, ivertexbuilder, -0.5f, 0.5f, 1, 0);
-        iconVertex(entry, ivertexbuilder, 0.5f, 0.5f, 0, 0);
-        matrix.popPose();
-    }
-
-    @OnlyIn(Dist.CLIENT)
-    private static void iconVertex(PoseStack.Pose entry, VertexConsumer builder, float x, float y, float u, float v) {
-        builder.vertex(entry.pose(), x, y, 0)
-                .uv(u, v)
-                .normal(entry.normal(), 0.0F, 1.0F, 0.0F)
-                .endVertex();
-    }
-
-    private static void addParticle(Level w, Vec3 vec, int r) {
-        float tpi = (float) (Math.PI * 2);
-        Vec3 v0 = new Vec3(0, r, 0);
-        Vec3 v1 = v0.xRot(tpi / 3).yRot((float) (Math.random() * tpi));
-        float a0 = (float) (Math.random() * tpi);
-        float b0 = (float) Math.acos(2 * Math.random() - 1);
-        v0 = v0.xRot(a0).yRot(b0);
-        v1 = v1.xRot(a0).yRot(b0);
-        w.addAlwaysVisibleParticle(ParticleRegistrate.EMERALD.get(),
-                vec.x + v0.x,
-                vec.y + v0.y,
-                vec.z + v0.z,
-                vec.x + v1.x,
-                vec.y + v1.y,
-                vec.z + v1.z);
     }
 
 }
