b8268035e414af3feae9db106ebd8e7c137598e6
lcy0x1 <arthurlcy0x1@gmail.com>
2022-02-17 14:00:14 -0800

    add color handler

--- src/main/java/dev/hikarishima/lightland/init/ClientRegister.java
+++ src/main/java/dev/hikarishima/lightland/init/ClientRegister.java
@@ -1,10 +1,19 @@
 package dev.hikarishima.lightland.init;
 
+import com.tterrag.registrate.util.entry.RegistryEntry;
 import dev.hikarishima.lightland.content.archery.item.GenericBowItem;
+import dev.hikarishima.lightland.content.burserker.item.MedicineArmor;
+import dev.hikarishima.lightland.init.registrate.ItemRegistrate;
 import net.minecraft.client.renderer.item.ItemProperties;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.item.alchemy.PotionUtils;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.client.event.ColorHandlerEvent;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class ClientRegister {
@@ -16,6 +25,17 @@ public class ClientRegister {
             ItemProperties.register(bow, new ResourceLocation("pull"), (stack, level, entity, i) -> entity == null || entity.getUseItem() != stack ? 0.0F : bow.getPullForTime(entity, stack.getUseDuration() - entity.getUseItemRemainingTicks()));
             ItemProperties.register(bow, new ResourceLocation("pulling"), (stack, level, entity, i) -> entity != null && entity.isUsingItem() && entity.getUseItem() == stack ? 1.0F : 0.0F);
         }
+    }
+
+    @OnlyIn(Dist.CLIENT)
+    public static void registerItemColors(ColorHandlerEvent.Item event) {
+        MedicineArmor[] items = Arrays.stream(ItemRegistrate.MEDICINE_ARMOR).map(RegistryEntry::get).toArray(MedicineArmor[]::new);
+        event.getItemColors().register((stack, val) -> {
+            if (val > 0) return -1;
+            List<MobEffectInstance> list = PotionUtils.getCustomEffects(stack);
+            if (list.size() != 1) return -1;
+            return list.get(0).getEffect().getColor();
+        }, items);
     }
 
 }
