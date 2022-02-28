aae5c9161c3c53eeea7a66263b262abbf234f22f
lcy0x1 <arthurlcy0x1@gmail.com>
2022-02-27 19:15:40 -0800

    add skllls

--- src/main/java/dev/hikarishima/lightland/content/skill/ImpactSkill.java
+++ src/main/java/dev/hikarishima/lightland/content/skill/ImpactSkill.java
@@ -1,4 +1,94 @@
 package dev.hikarishima.lightland.content.skill;
 
-public class ImpactSkill {
+import dev.hikarishima.lightland.compat.TeamAccessor;
+import dev.hikarishima.lightland.content.skill.internal.Skill;
+import dev.hikarishima.lightland.content.skill.internal.SkillConfig;
+import dev.hikarishima.lightland.content.skill.internal.SkillData;
+import dev.hikarishima.lightland.init.LightLand;
+import dev.lcy0x1.util.SerialClass;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.AABB;
+
+public class ImpactSkill extends Skill<ImpactSkill.Config, SkillData> {
+
+    @SerialClass
+    public static class Config extends SkillConfig<SkillData> {
+
+        @SerialClass.SerialField
+        public double[] radius;
+
+        @SerialClass.SerialField
+        public double[] impact;
+
+        @SerialClass.SerialField
+        public double[] damage;
+
+        @Override
+        public boolean isValid() {
+            if (!super.isValid()) return false;
+            if (radius.length != max_level) {
+                LightLand.LOGGER.error("radius length must be the same as max level");
+                return false;
+            }
+            if (impact.length != max_level) {
+                LightLand.LOGGER.error("impact length must be the same as max level");
+                return false;
+            }
+            if (damage.length != max_level) {
+                LightLand.LOGGER.error("damage length must be the same as max level");
+                return false;
+            }
+            for (double val : radius) {
+                if (val < 2 || val > 32) {
+                    LightLand.LOGGER.error("radius must be between 2 and 32");
+                    return false;
+                }
+            }
+            for (double val : impact) {
+                if (val < -3 || val > 3) {
+                    LightLand.LOGGER.error("radius must be between -3 and 3");
+                    return false;
+                }
+            }
+            for (double val : damage) {
+                if (val < 0 || val > 100) {
+                    LightLand.LOGGER.error("radius must be between 0 and 100");
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+
+    @Override
+    public void activate(Level level, ServerPlayer player, SkillData data) {
+        Config config = getConfig();
+        if (config != null) {
+            int lv = Math.min(config.max_level, data.level);
+            double radius = config.radius[lv];
+            double impact = config.radius[lv];
+            double damage = config.radius[lv];
+            DamageSource source = DamageSource.playerAttack(player);
+            for (Entity e : level.getEntities(player, new AABB(player.position(), player.position()).inflate(radius))) {
+                if (e.distanceToSqr(player) > radius * radius) continue;
+                if (e instanceof LivingEntity le && TeamAccessor.arePlayerAndEntityInSameTeam(player, le)) continue;
+                e.hasImpulse = true;
+                e.setDeltaMovement(e.getPosition(1).subtract(player.position()).normalize().scale(impact));
+                if (!(e instanceof LivingEntity le)) continue;
+                le.hurt(source, (float) damage);
+            }
+        }
+        super.activate(level, player, data);
+    }
+
+    @Override
+    public SkillData genData(Player player) {
+        return new SkillData();
+    }
+
 }
--- src/main/java/dev/hikarishima/lightland/init/special/SkillRegistry.java
+++ src/main/java/dev/hikarishima/lightland/init/special/SkillRegistry.java
@@ -3,6 +3,7 @@ package dev.hikarishima.lightland.init.special;
 import com.tterrag.registrate.util.entry.RegistryEntry;
 import com.tterrag.registrate.util.nullness.NonNullSupplier;
 import dev.hikarishima.lightland.content.skill.EffectSkill;
+import dev.hikarishima.lightland.content.skill.ImpactSkill;
 import dev.hikarishima.lightland.content.skill.MovementSkill;
 import dev.hikarishima.lightland.content.skill.internal.Skill;
 import dev.hikarishima.lightland.init.LightLand;
@@ -13,6 +14,11 @@ public class SkillRegistry {
     public static final RegistryEntry<EffectSkill> BLOOD_THIRST = reg("blood_thirst", EffectSkill::new);
     public static final RegistryEntry<MovementSkill> QUICK_MOVE = reg("quick_move", MovementSkill::new);
     public static final RegistryEntry<EffectSkill> QUICK_PULL = reg("quick_pull", EffectSkill::new);
+    public static final RegistryEntry<EffectSkill> TARGET_HIDE = reg("target_hide", EffectSkill::new);
+    public static final RegistryEntry<EffectSkill> TARGET_ATTRACT = reg("target_attract", EffectSkill::new);
+    public static final RegistryEntry<EffectSkill> ARMOR_BREAKER = reg("armor_breaker", EffectSkill::new);
+    public static final RegistryEntry<ImpactSkill> IMPACT_ATTRACT = reg("impact_attract", ImpactSkill::new);
+    public static final RegistryEntry<ImpactSkill> IMPACT_REPEL = reg("impact_repel", ImpactSkill::new);
 
     @SuppressWarnings({"rawtypes", "unchecked"})
     public static <T extends Skill<?, ?>> RegistryEntry<T> reg(String id, NonNullSupplier<T> sup) {
