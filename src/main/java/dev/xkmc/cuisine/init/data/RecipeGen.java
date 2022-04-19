08c00844417b6be15f166af2e110ce84f35b5591
lcy0x1 <arthurlcy0x1@gmail.com>
2022-04-18 17:00:52 -0700

    add wood recipes

--- src/main/java/dev/xkmc/cuisine/init/data/RecipeGen.java
+++ src/main/java/dev/xkmc/cuisine/init/data/RecipeGen.java
@@ -10,18 +10,36 @@ import dev.xkmc.cuisine.init.Cuisine;
 import dev.xkmc.cuisine.init.registrate.CuisineFluids;
 import dev.xkmc.cuisine.init.registrate.SimpleItem;
 import net.minecraft.advancements.critereon.InventoryChangeTrigger;
+import net.minecraft.data.recipes.ShapedRecipeBuilder;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraft.world.level.ItemLike;
 import net.minecraft.world.level.material.Fluids;
 import net.minecraftforge.fluids.FluidStack;
+import net.minecraftforge.registries.IForgeRegistryEntry;
 
 import java.util.function.BiFunction;
+import java.util.function.Supplier;
 
 public class RecipeGen {
 
 	public static void genRecipe(RegistrateRecipeProvider pvd) {
+		{
+			for (WoodType type : WoodType.values()) {
+				pvd.planks(DataIngredient.items(type.LOG.get()), type.PLANK);
+				pvd.slab(DataIngredient.items(type.PLANK.get()), type.SLAB, null, false);
+				pvd.stairs(DataIngredient.items(type.PLANK.get()), type.STAIR, null, false);
+				pvd.fence(DataIngredient.items(type.PLANK.get()), type.FENCE, null);
+				pvd.fenceGate(DataIngredient.items(type.PLANK.get()), type.FENCE_GATE, null);
+				pvd.trapDoor(DataIngredient.items(type.PLANK.get()), type.TRAPDOOR, null);
+				pvd.door(DataIngredient.items(type.PLANK.get()), type.DOOR, null);
+				pvd.singleItem(DataIngredient.items(type.PLANK.get()), type.BUTTON, 1, 1);
+				boat(pvd, DataIngredient.items(type.PLANK.get()), type.BOAT_ITEM);
+				pressurePlate(pvd, DataIngredient.items(type.PLANK.get()), type.PRESSURE_PLATE);
+			}
+		}
 		{
 			unlock(pvd, new PanRecipeBuilder(Items.RABBIT_STEW, 1, 100, Items.BOWL)::unlockedBy, Items.RABBIT_STEW)
 					.requires(new FluidStack(Fluids.WATER, 2)).requires(Items.BOWL).requires(Items.RABBIT)
@@ -70,6 +88,23 @@ public class RecipeGen {
 
 	private static <T> T unlock(RegistrateRecipeProvider pvd, BiFunction<String, InventoryChangeTrigger.TriggerInstance, T> func, Item item) {
 		return func.apply("has_" + pvd.safeName(item), DataIngredient.items(item).getCritereon(pvd));
+	}
+
+
+	public static <T extends ItemLike & IForgeRegistryEntry<?>> void boat(RegistrateRecipeProvider pvd, DataIngredient source, Supplier<? extends T> result) {
+		ShapedRecipeBuilder.shaped(result.get())
+				.pattern("X X").pattern("XXX")
+				.define('X', source)
+				.unlockedBy("has_" + pvd.safeName(source), source.getCritereon(pvd))
+				.save(pvd, pvd.safeId(result.get()));
+	}
+
+	public static <T extends ItemLike & IForgeRegistryEntry<?>> void pressurePlate(RegistrateRecipeProvider pvd, DataIngredient source, Supplier<? extends T> result) {
+		ShapedRecipeBuilder.shaped(result.get())
+				.pattern("XX")
+				.define('X', source)
+				.unlockedBy("has_" + pvd.safeName(source), source.getCritereon(pvd))
+				.save(pvd, pvd.safeId(result.get()));
 	}
 
 }
